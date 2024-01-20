b2ba8e93532e1c814b661d575d155bc0a8f94486
orellesha9 <orellesha9@gmail.com>
2024-01-20 19:56:33 +0200

    finish

--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -2,9 +2,9 @@
 const Statistics = ({ good, neutral, bad, total, feedback }) => {
   return (
     <>
-      <p>Good:{good}</p>
-      <p>Neutral:{neutral}</p>
-      <p>Bad:{bad}</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
       <p>Total: {total}</p>
       <p>Positive feedback: {feedback}%</p>
     </>
